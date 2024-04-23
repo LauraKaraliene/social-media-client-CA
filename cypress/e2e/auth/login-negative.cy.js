43067c9d5aecdd918ea5c30dbef2aa740b56980a
Laura Karaliene <Laura.Karaliene@stud.noroff.no>
2024-04-23 12:15:03 +0200

    updates login-negative.cy.js test

--- cypress/e2e/auth/login-negative.cy.js
+++ cypress/e2e/auth/login-negative.cy.js
@@ -5,16 +5,16 @@ describe('Login functionality - Negative Tests', () => {
     cy.get('#registerModal [data-auth="login"]').click()
     cy.wait(1000)
 
+    cy.get('input#loginEmail').type(Cypress.env('invalidEmail'))
+    cy.get('input#loginPassword').type(Cypress.env('invalidPassword'))
+    cy.get('#loginForm').submit()
+
     // Prepare to catch the alert
     cy.on('window:alert', (text) => {
       expect(text).to.contains(
         'Either your username was not found or your password is incorrect',
       )
     })
-
-    cy.get('input#loginEmail').type(Cypress.env('invalidEmail'))
-    cy.get('input#loginPassword').type(Cypress.env('invalidPassword'))
-    cy.get('#loginForm').submit()
 
     // ensure the alert was actually called by adding a fail-safe check
     cy.get('input#loginEmail').should('exist')
