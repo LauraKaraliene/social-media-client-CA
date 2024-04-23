332f50070f789b6e652233ebd194257829c5d5ee
Laura Karaliene <Laura.Karaliene@stud.noroff.no>
2024-04-23 13:08:58 +0200

    update unit test code

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -3,7 +3,6 @@ import MockLocalStorage from '../../mocks/MockLocalStorage.js'
 
 const ACCESS_TOKEN = '12345'
 
-// Create a mock fetch function that simulates a successful network call returning an access token
 const mockFetchSuccess = jest.fn().mockResolvedValue({
   ok: true,
   json: jest.fn().mockResolvedValue({ accessToken: ACCESS_TOKEN }),
@@ -11,20 +10,11 @@ const mockFetchSuccess = jest.fn().mockResolvedValue({
 
 describe('the login function', () => {
   it('stores a token when provided with valid credentials', async () => {
-    // Assign the mock fetch function to the global fetch function
     global.fetch = mockFetchSuccess
-
-    // Create an instance of MockLocalStorage and assign it to the global localStorage
     const mockStorage = new MockLocalStorage()
     global.localStorage = mockStorage
-
-    // Call the login function
     await login('email', 'password')
-
-    // Retrieve and parse the token from localStorage
     const storedToken = JSON.parse(global.localStorage.getItem('token'))
-
-    // Check that the token is stored correctly
     expect(storedToken).toEqual(ACCESS_TOKEN)
   })
 })
--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -3,14 +3,10 @@ import MockLocalStorage from '../../mocks/MockLocalStorage.js'
 
 describe('the logout function', () => {
   it('removes token from storage', () => {
-    // Create an instance of MockLocalStorage and assign it to the global localStorage
     const mockStorage = new MockLocalStorage()
     global.localStorage = mockStorage
-    // Add a token to mock localStorage to simulate a logged-in state
     mockStorage.setItem('token', '12345')
-    // Call the logout function
     logout()
-    // Check that 'token' has been removed from storage
     expect(global.localStorage.getItem('token')).toBeNull()
   })
 })
